05efec16ae91b4c797ef073489ef81b5539f9ca7
Brigitte Michau <brigitte@fontera.com>
2014-08-30 22:42:12 +0200

    Chapter 12 Touch Events and UIResponder. Drawing with BNRDrawView

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -7,7 +7,49 @@
 //
 
 #import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView ()
+@property (strong, nonatomic) BNRLine *currentLine;
+@property (strong, nonatomic) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDrawView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.finishedLines = [[NSMutableArray alloc]init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line {
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect {
+    // Draw finised lines in black
+    [[UIColor blackColor]set];
+    
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine) {
+        // If there is a line currently being drawn - do it in red
+        [[UIColor redColor]set];
+        [self strokeLine:self.currentLine];
+    }
+}
 
 @end
--- TouchTracker/Model/BNRLine.h
+++ TouchTracker/Model/BNRLine.h
@@ -10,7 +10,7 @@
 
 @interface BNRLine : NSObject
 
-@property (nonatomic) CGPoint start;
+@property (nonatomic) CGPoint begin;
 @property (nonatomic) CGPoint end;
 
 @end
