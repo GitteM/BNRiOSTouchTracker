96ec645b91b634a45834476f45814028995b6e8b
Brigitte Michau <brigitte@fontera.com>
2014-08-30 22:11:40 +0200

    Initial Commit

---
+++ TouchTracker/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  TouchTracker
+//
+//  Created by Brigitte Michau on 2014/08/30.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
